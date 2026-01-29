b36386e6880a7c93ecdf02500302104c8cf3ec67
Luke Rickard <rickard@robots.ox.ac.uk>
2026-01-29 09:12:22 +0000

    Working bottom-up full info

--- Coalition.jl
+++ Coalition.jl
@@ -19,6 +19,57 @@ function find_opt_coal(buildings::Vector{Building}, max_coal_size::Int)
     return sorted_coal_vals[1][1], sorted_coal_vals[1][2]
 end
 
+function bottom_up_full_info(buildings::Vector{Building}, max_coal_size::Int)
+    agents = Vector(1:length(buildings))
+    done = false
+    while !done
+        done = true
+        #for agent in agents
+        #    println(optimise(opt, buildings[agent])[1])
+        #end
+        outs = [optimise(opt, buildings[agent]) for agent in agents]
+        res = [out[1] for out in outs]
+        vars = [out[2] for out in outs]
+
+        poss_coals = collect(combinations(agents,2))
+        poss_coal_vals = Dict()
+        for c in poss_coals
+            poss_coal_vec = Vector()
+            append!(poss_coal_vec, c[1], c[2])
+            poss_coal_vals[c] = sum(objective_value(optimise(opt, buildings[poss_coal_vec])[1]))
+        end
+        sorted_coal_vals = sort!(collect(poss_coal_vals), by=last)
+        new_agents = Vector()
+        coaled_agents = Vector()
+        for elem in sorted_coal_vals
+            if !(elem[1][1] in coaled_agents) && !(elem[1][2] in coaled_agents)
+                new_coal = Vector()
+                append!(new_coal, elem[1][1], elem[1][2])
+                if length(new_coal) <= max_coal_size
+                    push!(new_agents, new_coal)
+                    push!(coaled_agents, elem[1][1], elem[1][2])
+                    done = false
+                end
+            end
+        end
+        for agent in agents
+            if !(agent in coaled_agents)
+                push!(new_agents, agent)
+            end
+        end
+
+        agents = new_agents
+        #println(sorted_coal_vals)
+        #update cons_vec
+    end
+    #println(cons_vec)
+    #println("cons ", value(vars[2][2]))
+    #println("sell ", vars[1][3])
+    outs = [optimise(opt, buildings[agent]) for agent in agents]
+    res = [sum(objective_value(out[1])) for out in outs]
+    return agents, sum(res)
+end
+
 function privacy_focussed_coals(buildings::Vector{Building}, max_coal_size::Int)
     agents = Vector(1:length(buildings))
     done = false
@@ -36,8 +87,10 @@ function privacy_focussed_coals(buildings::Vector{Building}, max_coal_size::Int)
         poss_coals = collect(combinations(agents,2))
         poss_coal_vals = Dict()
         for c in poss_coals
-            if any(cons_vec[c[1]].*cons_vec[c[2]] < zeros(length(cons_vec[c[1]])))
-                poss_coal_vals[c] = norm(cons_vec[c[1]]+cons_vec[c[2]])
+            compatible_slots = cons_vec[c[1]].*cons_vec[c[2]] .< zeros(length(cons_vec[c[1]]))
+            if any(compatible_slots)
+                poss_coal_vals[c] = sum(min(abs.(compatible_slots.*cons_vec[c[1]]),abs.(compatible_slots.*cons_vec[c[2]])))
+                #poss_coal_vals[c] = norm(cons_vec[c[1]]+cons_vec[c[2]])
             end
         end
         sorted_coal_vals = sort!(collect(poss_coal_vals), by=last)
--- test.jl
+++ test.jl
@@ -11,7 +11,7 @@ include("load_EMS_data.jl")
 # energy_sale = 0.5*energy_cost 
 
 
-num_builds = 10
+num_builds = 70
 #buildings = [Building((rand(Float64, 1)[1], rand(Float64, 1)[1]), rand(Float64, 24), rand(Float64, 24), rand(Float16, 1)[1],rand(Float16, 1)[1],rand(Float16, 1)[1],rand(Float16, 1)[1],i) for i = 1:num_builds]
 buildings, energy_cost, energy_sale = load_from_CSV(num_builds)
 opt = MPC_optimiser(energy_cost', energy_sale')
@@ -51,20 +51,35 @@ println("Computation took ", t4-t3,"s")
 
 println("-------------")
 
-println("Coalitional optimal")
-
-t5 = time()
-opt_coal, val = find_opt_coal(buildings,3)
-t6 = time()
-
-println("In optimal coalition ",opt_coal, " agents pay ", val)
-println("Computation took ", t6-t5,"s")
-
+max_coal_size = 4
 
 t7 = time()
-private_coal, val = privacy_focussed_coals(buildings,3)
+private_coal, val = privacy_focussed_coals(buildings,max_coal_size)
 t8 = time()
 
 println("In private coalition ",private_coal, " agents pay ", val)
 println("Computation took ", t8-t7,"s")
+
+println("-------------")
+
+t9 = time()
+private_coal, val = bottom_up_full_info(buildings,max_coal_size)
+t10 = time()
+
+println("In full-info bottom-up coalition ",private_coal, " agents pay ", val)
+println("Computation took ", t10-t9,"s")
+println("-------------")
+
+
+if num_builds <= 8 #takes about 20 mins on laptop to execute 8
+    println("Coalitional optimal")
+
+    t5 = time()
+    opt_coal, val = find_opt_coal(buildings,max_coal_size)
+    t6 = time()
+
+    println("In optimal coalition ",opt_coal, " agents pay ", val)
+    println("Computation took ", t6-t5,"s")
+end
+
 
